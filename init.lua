478795c19146d37bdacf1d527e0198597506c00e
CristianMor <Cristian.moreno@acuicultec.mx>
2022-12-18 11:45:23 -0700

    Init lua se limpio

--- init.lua
+++ init.lua
@@ -8,17 +8,3 @@ require "cr1xsh0.core.keymaps"
 require "cr1xsh0.core.colorscheme"
 --
 
---
-require "cr1xsh0.plugins"
-require "cr1xsh0.feline"
-require "cr1xsh0.cmp"
-require "cr1xsh0.lsp"
-require "cr1xsh0.telescope"
-require "cr1xsh0.treesitter"
-require "cr1xsh0.auto-pairs"
-require "cr1xsh0.comment"
-require "cr1xsh0.gitsigns"
-require "cr1xsh0.nvim-tree"
-require "cr1xsh0.buffline"
-require "cr1xsh0.null-ls"
-require "cr1xsh0.dashboard"
