b1851e461a297668bd9622ee9d14ee78ba01fe1b
CristianMor <Cristian.moreno@acuicultec.mx>
2023-01-03 15:07:46 -0700

    Cambio icon de code action lspsaga

--- lua/cr1xsh0/plugins/lsp/lspsaga.lua
+++ lua/cr1xsh0/plugins/lsp/lspsaga.lua
@@ -1,20 +1,14 @@
 local lspsaga_status, saga = pcall(require, "lspsaga")
-
 if not lspsaga_status then
   print "Exite un problema con lspsaga"
   return
 end
 
 saga.init_lsp_saga({
-  move_in_saga = {
-    prev = "<C-k>",
-    next = "<C-j>"
-  },
-  finder_action_keys = {
-    open = "<CR>"
-  },
-  definition_action_keys = {
-    edit = "<CR>"
-  }
+  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+  diagnostic_header = { "😡 ", "😥 ", "😤 ", "😐 " },
+  finder_action_keys = { open = "<CR>" },
+  definition_action_keys = { edit = "<CR>" },
+  code_action_icon = "🔥"
 })
 
