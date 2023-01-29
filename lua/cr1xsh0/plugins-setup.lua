8ec51bbd88897a46d845c20b9ffff0bfd4e6bafe
CristianMor <Cristian.moreno@acuicultec.mx>
2023-01-28 19:12:48 -0700

    Markdown preview

--- lua/cr1xsh0/plugins-setup.lua
+++ lua/cr1xsh0/plugins-setup.lua
@@ -94,6 +94,23 @@ return packer.startup(function(use)
 	use("windwp/nvim-autopairs")
 	use("windwp/nvim-ts-autotag")
 
+	-- MarkdownPreview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+	--
 	-- Establecer automaticamente su configuracion despues de clonar packer.nvim
 	-- poner esto al dinal despues de todos los plugins
 	if PACKER_BOOTSTRAP then
