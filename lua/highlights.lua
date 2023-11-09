b40cec95a48c3a3ef13bf2f16a536f6e5a5e8ab9
CristianMor <Cristian.moreno@acuicultec.mx>
2023-11-09 13:23:00 -0700

    Agregando comentarios al archivo highlight

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,6 +1,17 @@
+-- Habilitar la resaltación de la línea del cursor
 vim.opt.cursorline = true
+
+-- Habilitar colores de terminal de 24 bits
 vim.opt.termguicolors = true
+
+-- Configurar la opacidad de las ventanas flotantes a 0 (sin opacidad)
 vim.opt.winblend = 0
+
+-- Configurar las opciones de autocompletado de comandos
 vim.opt.wildoptions = 'pum'
+
+-- Establecer la opacidad de la ventana emergente del autocompletado a 5
 vim.opt.pumblend = 5
+
+-- Establecer el fondo del entorno Vim a 'dark' (oscuro)
 vim.opt.background = 'dark'
