71bb10f23e7cff5e46b1eece2c7387b87056ee45
CristianMor <cristianmor706@gmail.com>
2024-09-06 12:15:41 -0700

    fix(null-ls): misspelled variable name

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -2,7 +2,7 @@ local status, null_ls = pcall(require, 'null-ls')
 if (not status) then return end
 
 null_ls.setup {
-  on_attach = function (cilent, bufnr)
+  on_attach = function (client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_command [[augroup Format]]
       vim.api.nvim_command [[autocmd! * <buffer>]]
